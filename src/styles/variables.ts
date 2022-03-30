454b6841ce49d0ee755be5686d906c2bd67bd757
manak1 <mana23happy@hotmail.co.jp>
2022-03-31 01:24:28 +0900

    🌟 feat: 共通利用するスタイル変数を定義するファイルを追加

---
+++ src/styles/variables.ts
@@ -0,0 +1,11 @@
+export const fontSizes = {
+  large: "24px",
+  medium: "20px",
+  default: "16px",
+  small: "14px",
+  xsmall: "12px",
+} as const
+
+export const colors = {
+  error: "#FF5656",
+} as const
