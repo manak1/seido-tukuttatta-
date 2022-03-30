a5d442bbcf503e2d7dcf4596cac4e4f744b82f44
manak1 <mana23happy@hotmail.co.jp>
2022-03-31 01:07:30 +0900

    🔧 fix: storybookの警告がでないように修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  babel: {
+    plugins: [["@babel/plugin-proposal-private-methods", { loose: true }]],
+  },
 }
 
 module.exports = nextConfig
