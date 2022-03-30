793d4fd70d79f7b440589dd3097d1b3cecc14a10
manak1 <mana23happy@hotmail.co.jp>
2022-03-31 01:22:35 +0900

    🐧 chore: jestがaliasを認識できるように変更

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,9 @@ const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/src/$1",
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
