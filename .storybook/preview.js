eeb4ea9338bcc3bb3581559960b696960ed1bdc8
manak1 <mana23happy@hotmail.co.jp>
2022-03-31 18:19:40 +0900

    🔧 fix: 各StoryがThemProviderでWrapされるように変更

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,16 @@
+import { ThemeProvider } from "@emotion/react"
+import { theme } from "~/styles/theme"
+
+export const decorators = [
+  (Story) => {
+    return (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    )
+  },
+]
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
