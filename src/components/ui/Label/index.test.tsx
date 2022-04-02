8ec5b1515834504afd677db0dc3708a4b1910853
manak1 <mana23happy@hotmail.co.jp>
2022-04-03 04:08:48 +0900

    🎨 style: test名の修正

--- src/components/ui/Label/index.test.tsx
+++ src/components/ui/Label/index.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "~/test/test-utils"
 import Label from "./index"
 
-describe("InputTextのテスト", () => {
+describe("Labelのテスト", () => {
   it("snapshot", () => {
     const { asFragment } = render(<Label label="ラベル" />)
     expect(asFragment()).toMatchSnapshot()
