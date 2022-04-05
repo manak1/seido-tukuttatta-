46fcc6bdbe0736b3eafe2da52c7cf56525097270
manak1 <mana23happy@hotmail.co.jp>
2022-04-06 00:17:29 +0900

    🌟 feat: headerからホームに飛べるようにリンクを追加

--- src/components/ui/Header/index.style.ts
+++ src/components/ui/Header/index.style.ts
@@ -5,13 +5,18 @@ export const Header = styled.header(
     display: "flex",
   },
   ({ theme }) => ({
-    backgroundColor: theme.colors.primary,
+    backgroundColor: theme.colors.white,
     padding: "8px 16px",
+    borderBottom: `1px solid ${theme.colors.gray}`,
   })
 )
 
 export const Title = styled.h1({}, ({ theme }) => ({
-  color: theme.colors.white,
+  color: theme.colors.black,
   fontSize: theme.fontSizes.medium,
   margin: 0,
 }))
+
+export const Link = styled.a({
+  textDecoration: "none",
+})
--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Link from "next/link"
 import * as Styled from "./index.style"
 
 type HeaderProps = React.ComponentProps<"header">
@@ -6,7 +7,11 @@ type HeaderProps = React.ComponentProps<"header">
 const Header: React.VFC<HeaderProps> = (props) => {
   return (
     <Styled.Header {...props}>
-      <Styled.Title>制度作ってみたったー</Styled.Title>
+      <Link href="/" passHref>
+        <Styled.Link>
+          <Styled.Title>制度作ってみたったー</Styled.Title>
+        </Styled.Link>
+      </Link>
     </Styled.Header>
   )
 }
