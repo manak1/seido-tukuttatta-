c6f24dd63d72f68b057cb260e4d84c7687ad9764
manak1 <mana23happy@hotmail.co.jp>
2022-04-05 23:16:04 +0900

    🌟 feat: headerのstoryを追加

---
+++ src/components/ui/Header/index.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+
+import Component from "./index"
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Header",
+  component: Component,
+} as ComponentMeta<typeof Component>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Component> = (args) => {
+  return <Component {...args} />
+}
+
+export const Default = Template.bind({})
