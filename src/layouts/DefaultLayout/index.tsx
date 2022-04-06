c560e2a72aeff0b22cbbc7a805adfd1edf1f7a4a
manak1 <mana23happy@hotmail.co.jp>
2022-04-06 19:10:16 +0900

    🌟 feat: 基本layoutコンポーネントの作成

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,16 +1,34 @@
-import React from "react"
+import React, { useCallback } from "react"
 import Header from "~/components/ui/Header"
 import Wrapper from "~/components/ui/Wrapper"
 import NavBottom from "~/components/ui/NavBottom"
 import { css } from "@emotion/react"
 import { navLinks } from "~/constants/navbar"
+import IconButton from "~/components/ui/IconButton"
+import { Add } from "emotion-icons/material"
+import { useRouter } from "next/router"
 
 const DefaultLayout: React.FC = (props) => {
   const { children } = props
+
+  const router = useRouter()
+
+  const jumpToCreatePage = useCallback(() => {
+    router.push("s/create")
+  }, [router])
+
   return (
     <>
       <Header />
-      <Wrapper>{children}</Wrapper>
+      <Wrapper
+        css={css`
+          border-left: 1px solid #d3d3d3;
+          border-right: 1px solid #d3d3d3;
+          min-height: 100vh;
+        `}
+      >
+        {children}
+      </Wrapper>
       <NavBottom
         links={navLinks}
         css={css`
@@ -19,7 +37,18 @@ const DefaultLayout: React.FC = (props) => {
           margin: 0;
           bottom: 0;
         `}
-      />
+      >
+        <IconButton
+          icon={Add}
+          onClick={jumpToCreatePage}
+          css={css`
+            position: absolute;
+            right: 5%;
+            bottom: 64px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.26);
+          `}
+        />
+      </NavBottom>
     </>
   )
 }
---
+++ src/layouts/DetailLayout/index.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import HeaderBack from "~/components/ui/HeaderBack"
+import Wrapper from "~/components/ui/Wrapper"
+import NavBottom from "~/components/ui/NavBottom"
+import { css } from "@emotion/react"
+import { navLinks } from "~/constants/navbar"
+
+const DefaultLayout: React.FC = (props) => {
+  const { children } = props
+  return (
+    <>
+      <HeaderBack title="新規作成" />
+      <Wrapper>{children}</Wrapper>
+      <NavBottom
+        links={navLinks}
+        css={css`
+          position: fixed;
+          left: 0;
+          margin: 0;
+          bottom: 0;
+        `}
+      />
+    </>
+  )
+}
+
+export default DefaultLayout
