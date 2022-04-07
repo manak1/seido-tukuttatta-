4e1842ea17a8f25e96c29b96c4108a7a2efe9b7f
manak1 <mana23happy@hotmail.co.jp>
2022-04-08 00:53:09 +0900

    🌟 feat: wrapperにposition relativeを指定

--- src/components/ui/Wrapper/index.style.ts
+++ src/components/ui/Wrapper/index.style.ts
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled"
 
 export const Wrapper = styled.div({}, ({ theme }) => ({
+  position: "relative",
   width: "100%",
   maxWidth: theme.maxWidth.default,
   padding: "0 16px",
