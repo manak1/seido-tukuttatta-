7eea1e55106c6ae17a0b10f3e89ceadf6961d6e8
manak1 <mana23happy@hotmail.co.jp>
2022-04-08 01:34:56 +0900

    ♻️ refactor: textAreaのprops名を具体的なものに変更

--- src/components/ui/TextArea/index.tsx
+++ src/components/ui/TextArea/index.tsx
@@ -2,24 +2,26 @@ import React, { forwardRef } from "react"
 import Label from "~/components/ui/Label"
 import * as Styled from "./index.style"
 
-type Props = React.ComponentProps<"textarea"> & {
+export type TextAreaProps = React.ComponentProps<"textarea"> & {
   name: string
   label: string
   isOptional?: boolean
 }
 
-const TextArea = forwardRef<HTMLTextAreaElement, Props>((props, ref) => {
-  const { name, label, isOptional, ...rest } = props
-  return (
-    <Styled.Wrapper>
-      <Styled.Box>
-        <Label label={label} htmlFor={name} />
-        {isOptional && <Styled.OptionalText>任意</Styled.OptionalText>}
-      </Styled.Box>
-      <Styled.TextArea ref={ref} id={name} {...rest} />
-    </Styled.Wrapper>
-  )
-})
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  (props, ref) => {
+    const { name, label, isOptional, ...rest } = props
+    return (
+      <Styled.Wrapper>
+        <Styled.Box>
+          <Label label={label} htmlFor={name} />
+          {isOptional && <Styled.OptionalText>任意</Styled.OptionalText>}
+        </Styled.Box>
+        <Styled.TextArea ref={ref} id={name} {...rest} />
+      </Styled.Wrapper>
+    )
+  }
+)
 TextArea.displayName = "TextArea"
 
 export default TextArea
