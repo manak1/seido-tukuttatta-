8174f42b89c2ada1f7751d13f300c494a69b3d5f
manak1 <mana23happy@hotmail.co.jp>
2022-04-11 01:08:35 +0900

    🌟 feat: selectの選択肢をまとめて定義するファイルを追加

---
+++ src/constants/select.ts
@@ -0,0 +1,12 @@
+import { SelectItem } from "~/components/ui/Select"
+
+export const thumbnailItems: SelectItem[] = [
+  {
+    value: "1",
+    label: "シンプル",
+  },
+  {
+    value: "2",
+    label: "チーム",
+  },
+]
