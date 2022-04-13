fb5b518576461bda8afd1721deb964b64eb556b7
manak1 <mana23happy@hotmail.co.jp>
2022-04-13 23:59:19 +0900

    🌟 feat: portal用にdivを追加

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document"
+import React from "react"
+
+const Docuemnt = () => {
+  return (
+    <Html lang="ja">
+      <Head />
+      <body>
+        <Main />
+        <div id="modal-portal" />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Docuemnt
