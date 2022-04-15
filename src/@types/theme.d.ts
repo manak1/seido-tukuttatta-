1baf3aa2e67cc6412c332d92ee5f4b926dc8de94
manak1 <mana23happy@hotmail.co.jp>
2022-04-15 23:28:11 +0900

    ♻️ refactor: themeの型を改善

--- src/@types/theme.d.ts
+++ src/@types/theme.d.ts
@@ -2,13 +2,6 @@ import "@emotion/react"
 import { theme } from "~/styles/theme"
 
 type CustomTheme = typeof theme
-
 declare module "@emotion/react" {
-  export interface Theme {
-    fontSizes: CustomTheme["fontSizes"]
-    colors: CustomTheme["colors"]
-    focus: CustomTheme["focus"]
-    zIndex: CustomTheme["zIndex"]
-    maxWidth: CustomTheme["maxWidth"]
-  }
+  export interface Theme extends CustomTheme {}
 }
