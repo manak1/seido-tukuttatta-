59ace8edc3c624555cb2722ea8176ea07bf99502
manak1 <mana23happy@hotmail.co.jp>
2022-04-16 01:53:13 +0900

    🌟 feat: 制度作成時に表示するモーダルを作成

---
+++ src/components/model/companySystem/CreatedCompanySystemModal/index.tsx
@@ -0,0 +1,37 @@
+import React, { useMemo } from "react"
+
+import Button from "~/components/ui/Button"
+import Modal, { ModalProps } from "~/components/ui/Modal"
+
+import { CompanySystem } from "~/@types"
+
+import * as Styled from "./index.style"
+
+type CreatedCompanySystemModalProps = ModalProps & {
+  companySystem: CompanySystem
+}
+
+const CreatedSystemCompanyModal: React.VFC<CreatedCompanySystemModalProps> = (
+  props
+) => {
+  const { onClose, isOpen, domId, companySystem } = props
+
+  const createMessage = useMemo(() => {
+    return `株式会社ぺんぎんにあたらしい制度
+    ${companySystem.name} が誕生しました。
+   素晴らしい制度の誕生をTwitterで社員の皆さんに知らせててあげてください。`
+  }, [companySystem.name])
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} domId={domId}>
+      <Styled.Title>おめでとうございます！！ </Styled.Title>
+      <Styled.Message>{createMessage}</Styled.Message>
+      <Styled.Buttons>
+        <Button onClick={onClose} label="制度を共有する" isFullWidth />
+        <Button onClick={onClose} label="閉じる" variant="cancel" isFullWidth />
+      </Styled.Buttons>
+    </Modal>
+  )
+}
+
+export default CreatedSystemCompanyModal
