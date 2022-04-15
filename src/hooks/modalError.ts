494fa4c6ec7f01776e04947ebe4fa234c31bde61
manak1 <mana23happy@hotmail.co.jp>
2022-04-15 23:30:26 +0900

    ♻️ refactor: modalError hookを改善

--- src/hooks/modalError.ts
+++ src/hooks/modalError.ts
@@ -1,24 +1,26 @@
+import { useMemo } from "react"
 import { useRecoilCallback, useRecoilValue } from "recoil"
 
 import { errorsAtom, errorSelector, ModalError } from "~/store/errorts"
 
 export const useModalError = () => {
-  const useGetError = () => {
-    const errors = useRecoilValue(errorSelector)
-    return errors
-  }
+  const error = useRecoilValue(errorSelector)
 
-  const useAddError = useRecoilCallback(({ set }) => (error: ModalError) => {
+  const getError = useMemo(() => {
+    return error
+  }, [error])
+
+  const addError = useRecoilCallback(({ set }) => (error: ModalError) => {
     set(errorsAtom, (errors) => [...errors, error])
   })
 
-  const usePopError = useRecoilCallback(({ set }) => () => {
+  const popError = useRecoilCallback(({ set }) => () => {
     set(errorsAtom, (errors) => errors.slice(1))
   })
 
   return {
-    useGetError,
-    useAddError,
-    usePopError,
+    getError,
+    addError,
+    popError,
   }
 }
