e8769887fe459d6fd0d342a07d57ca50ed4930f4
manak1 <mana23happy@hotmail.co.jp>
2022-04-16 15:26:26 +0900

    🔧 fix: middleware修正

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,23 +1,21 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export function middleware(req: NextRequest) {
-  if (process.env.NODE_ENV === "production") {
-    const basicAuth = req.headers.get("authorization")
+  const basicAuth = req.headers.get("authorization")
 
-    if (basicAuth) {
-      const auth = basicAuth.split(" ")[1]
-      const [user, pwd] = Buffer.from(auth, "base64").toString().split(":")
+  if (basicAuth) {
+    const auth = basicAuth.split(" ")[1]
+    const [user, pwd] = Buffer.from(auth, "base64").toString().split(":")
 
-      if (user === "manaki" && pwd === "penguin") {
-        return NextResponse.next()
-      }
+    if (user === "manaki" && pwd === "penguin") {
+      return NextResponse.next()
     }
-
-    return new Response("ログインしてね", {
-      status: 401,
-      headers: {
-        "WWW-Authenticate": 'Basic realm="Secure Area"',
-      },
-    })
   }
+
+  return new Response("ログインしてね", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Secure Area"',
+    },
+  })
 }
