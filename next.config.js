bff5f8f9906a0ecd6ec5f23f5bbe7c65e2ed8c08
manak1 <mana23happy@hotmail.co.jp>
2022-04-16 14:16:38 +0900

    🔧 fix: styleファイルがpageとして認識されていたので対象の拡張子を変更

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["tsx"],
   reactStrictMode: true,
   babel: {
     plugins: [["@babel/plugin-proposal-private-methods", { loose: true }]],
