908ac43dbda5583e05d12ab8ad717b67896cce98
manak1 <mana23happy@hotmail.co.jp>
2022-04-16 19:03:30 +0900

    ♻️ refactor: useBool を使う形で置き換え

--- src/hooks/api/companySystem.ts
+++ src/hooks/api/companySystem.ts
@@ -1,31 +1,29 @@
 import { useCallback } from "react"
-import { useState } from "react"
 
 import { CompanySystem, CreateCompanySystem } from "~/@types"
 import { useAxios } from "~/hooks/api/axios"
+import { useBoolean } from "~/hooks/boolean"
 
 import { ApiResponseType } from "~/@types/api"
 
 export const usePostCompanySystem = () => {
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setLoadingTrue, setLoadingFalse] = useBoolean(false)
   const client = useAxios()
 
   const post = useCallback(
     async (data: CreateCompanySystem) => {
       if (isLoading) return
-      setIsLoading(true)
-
+      setLoadingTrue()
       const res = await client.post<ApiResponseType<CompanySystem>>(
         "/api/companySystem",
         data
       )
-      setIsLoading(false)
-
+      setLoadingFalse()
       if (res && res.data.success) {
         return res.data.data
       }
     },
-    [client, isLoading]
+    [client, isLoading, setLoadingFalse, setLoadingTrue]
   )
 
   return {
