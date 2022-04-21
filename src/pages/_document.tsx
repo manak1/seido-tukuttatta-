a151388c96b74fd5c457b456676259175e342ae4
manak1 <mana23happy@hotmail.co.jp>
2022-04-22 02:23:06 +0900

    🌟 feat: noto sans jp を読み込む

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,12 @@ import React from "react"
 const Docuemnt = () => {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <div id="modal-portal" />
