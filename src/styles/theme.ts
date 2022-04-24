68490791dbf521277eba302796ac4bfdceed4adf
manak1 <mana23happy@hotmail.co.jp>
2022-04-24 23:48:13 +0900

    🌟 feat: themeに色々追加

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -15,6 +15,9 @@ export const theme = {
     white: "#fff",
     gray: "#d3d3d3",
   },
+  letterSpacing: {
+    wide: "0.025em",
+  },
   maxWidth: {
     full: "100%",
     modal: "420px",
@@ -24,6 +27,14 @@ export const theme = {
     default: 1,
     overlay: 5,
     modal: 10,
+  },
+  shadow: {
+    base: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+    md: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+    lg: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+  },
+  mq: {
+    md: "@media (min-width: 580px)",
   },
   focus: (extraStyle?: CSSObject) => ({
     transition: "box-shadow 0.2s ease",
