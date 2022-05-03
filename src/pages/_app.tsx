94976daf4843d6ace2d3f0b7c1f4d95bfdf1fdd9
manak1 <mana23happy@hotmail.co.jp>
2022-05-04 00:49:43 +0900

    🌟 feat: swrConfigコンポーネントの配置

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider, Global } from "@emotion/react"
 import { RecoilRoot } from "recoil"
 
 import ErrorModalControl from "~/components/functional/ErrorModalControl"
+import SwrConfig from "~/components/functional/SwrConfig"
 
 import { globalStyle } from "~/styles/global"
 import { theme } from "~/styles/theme"
@@ -13,11 +14,13 @@ import "sanitize.css"
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <ErrorModalControl />
-        <Global styles={globalStyle} />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <SwrConfig>
+        <ThemeProvider theme={theme}>
+          <ErrorModalControl />
+          <Global styles={globalStyle} />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SwrConfig>
     </RecoilRoot>
   )
 }
