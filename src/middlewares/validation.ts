8703b7f4e81e0df190c5653f55e9c597e47bbc45
manak1 <mana23happy@hotmail.co.jp>
2022-05-06 06:12:07 +0900

    🔧 fix: post以外の時はvalidation処理を実行しないように変更

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -13,6 +13,7 @@ export const validationMiddleware = (schema: object) => {
     res: NextApiResponse<ApiErrorResponseType>,
     next: NextHandler
   ) => {
+    if (req.method !== "POST") return next()
     const isValid = validate(req.body)
     if (isValid) {
       return next()
