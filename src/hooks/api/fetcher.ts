533b9e192776a61ecae77af13bb0c4798536b8bd
manak1 <mana23happy@hotmail.co.jp>
2022-05-07 16:29:53 +0900

    🌟 feat: fetcherにエラー処理を追加

--- src/hooks/api/fetcher.ts
+++ src/hooks/api/fetcher.ts
@@ -2,7 +2,17 @@ import { useCallback } from "react"
 
 export const useFetcher = () => {
   const fetcher = useCallback((url: string, fetchArg?: RequestInit) => {
-    return fetch(url, fetchArg).then((res) => res.json())
+    return fetch(url, fetchArg).then(async (res) => {
+      if (!res.ok) {
+        const errorData = await res.json()
+        const error = new Error(
+          errorData.message ??
+            "何らかのエラーが発生しました。しばらくお待ちください。"
+        )
+        throw error
+      }
+      return res.json()
+    })
   }, [])
   return fetcher
 }
