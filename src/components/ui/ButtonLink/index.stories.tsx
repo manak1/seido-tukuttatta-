9ebd3a20cf0f9033bcfbb710f05c6980ad8c0567
manak1 <mana23happy@hotmail.co.jp>
2022-05-21 00:05:03 +0900

    🌟 feat: buttonLinkコンポーネントの作成

---
+++ src/components/ui/ButtonLink/index.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+
+import Component from "./index"
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "ui/ButtonLink",
+  component: Component,
+  argTypes: {
+    label: { control: "text", defaultValue: "ボタンだよ" },
+    isFullWidth: { control: "boolean", defaultValue: false },
+  },
+} as ComponentMeta<typeof Component>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Component> = (args) => {
+  return <Component {...args}>ボタン</Component>
+}
+
+export const Primary = Template.bind({})
+Primary.args = {}
+export const Danger = Template.bind({})
+Danger.args = {
+  variant: "danger",
+}
+export const Cancel = Template.bind({})
+Cancel.args = {
+  variant: "cancel",
+}
---
+++ src/components/ui/ButtonLink/index.style.ts
@@ -0,0 +1,85 @@
+import { Theme } from "@emotion/react"
+import styled from "@emotion/styled"
+
+import { ButtonLinkProps, ButtonLinkSize, ButtonLinkVariant } from "./index"
+
+export const ButtonLink = styled.a<Omit<ButtonLinkProps, "label">>(
+  {
+    minWidth: "36px",
+    outline: "none",
+    padding: "10px 16px",
+    border: "none",
+    borderRadius: "8px",
+    cursor: "pointer",
+    textDecoration: "none",
+    textAlign: "center",
+  },
+  ({ variant, isFullWidth, theme, size, icon }) => ({
+    fontSize: theme.fontSizes.default,
+    width: isFullWidth ? "100%" : "auto",
+    display: icon ? "flex" : "inline-block",
+    alignItems: icon ? "center" : "normal",
+    justifyContent: "center",
+    fontWeight: "bold",
+    "&:hover": {
+      filter: "brightness(95%)",
+      transition: "filter 0.2s ease-out",
+    },
+    ...setVariant(variant ?? "primary", theme),
+    ...theme.focus(),
+    ...setSize(size ?? "default"),
+    transition: "box-shadow, filter, 0.2s ease-in",
+  })
+)
+
+export const Text = styled.p({
+  margin: "0",
+  padding: "0",
+})
+
+function setVariant(variant: ButtonLinkVariant, theme: Theme) {
+  switch (variant) {
+    case "primary":
+      return {
+        backgroundColor: theme.colors.primary,
+        color: "#fff",
+      }
+    case "outline": {
+      return {
+        backgroundColor: theme.colors.white,
+        border: `3px solid ${theme.colors.primary}`,
+        color: theme.colors.primary,
+      }
+    }
+    case "danger":
+      return {
+        backgroundColor: theme.colors.error,
+        color: "#fff",
+      }
+    case "cancel":
+      return {
+        backgroundColor: "#fff",
+        color: theme.colors.black,
+        border: "1px solid #d3d3d3",
+      }
+    case "twitter":
+      return {
+        backgroundColor: theme.colors.blue,
+        color: theme.colors.white,
+      }
+  }
+}
+
+function setSize(buttonSize: ButtonLinkSize) {
+  switch (buttonSize) {
+    case "default":
+      return {
+        padding: "10px 16px",
+      }
+
+    case "small":
+      return {
+        padding: "8px 16px",
+      }
+  }
+}
