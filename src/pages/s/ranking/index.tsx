5ec207cc3751ba389b675041c9a3caa3bd4fa5b9
manak1 <mana23happy@hotmail.co.jp>
2022-06-18 01:54:20 +0900

    🌟 feat: ランキングページで制度を表示

--- src/pages/s/ranking/index.tsx
+++ src/pages/s/ranking/index.tsx
@@ -1,13 +1,43 @@
+import { useCallback } from "react"
+
+import Button from "~/components/ui/Button"
+import Title from "~/components/ui/Title"
+
+import LoadingContent from "~/components/functional/LoadingContent"
 import Spacer from "~/components/functional/Spacer"
 
 import DefaultLayout from "~/layouts/DefaultLayout"
 
+import CompanySystemRanking from "~/components/model/companySystem/CompanySystemRanking"
+import { useGetInfinityCompanySystemRanking } from "~/hooks/api/companySystem"
+
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
+  const { data, isLoading, isEnd, setSize, size } =
+    useGetInfinityCompanySystemRanking()
+
+  const loadMore = useCallback(() => {
+    setSize(size + 1)
+  }, [setSize, size])
+
   return (
     <DefaultLayout>
-      <Spacer axis="vertical" size={24} />
+      <Title>#ランキング</Title>
+      <Spacer axis="vertical" size={12} />
+      <LoadingContent isLoading={isLoading}>
+        <CompanySystemRanking companySystems={data ?? []} />
+      </LoadingContent>
+      <Spacer axis="vertical" size={16} />
+      <Button
+        isFullWidth
+        variant="outline"
+        size="small"
+        onClick={loadMore}
+        disabled={isEnd}
+      >
+        もっとみる
+      </Button>
     </DefaultLayout>
   )
 }
