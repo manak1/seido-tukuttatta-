a295998fb11df60701c13581b4c3081a836d08cc
manak1 <mana23happy@hotmail.co.jp>
2022-06-18 17:22:11 +0900

    🌟 feat: iconcheckのstory追加

--- src/components/ui/IconCheck/index.stories.tsx
+++ src/components/ui/IconCheck/index.stories.tsx
@@ -3,9 +3,15 @@ import { ComponentStory, ComponentMeta } from "@storybook/react"
 import Component from "./index"
 
 export default {
-  title: "",
+  title: "ui/IconCheck",
   component: Component,
-  argTypes: {},
+  argTypes: {
+    disabled: { type: "boolean", defaultValue: true },
+    value: { type: "boolean", defaultValue: false },
+    icon: { type: "string", defaultValue: "thumbsUp" },
+    id: { type: "string", defaultValue: "iconcheck" },
+    children: { type: "string", defaultValue: "12" },
+  },
 } as ComponentMeta<typeof Component>
 
 const Template: ComponentStory<typeof Component> = (args) => {
