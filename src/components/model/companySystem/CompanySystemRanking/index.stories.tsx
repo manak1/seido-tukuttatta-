7670f913f512a7e87bcaede1279ce1cb872a59d7
manak1 <mana23happy@hotmail.co.jp>
2022-06-19 20:07:45 +0900

    🔧 fix: storyで発生している型エラーの修正

--- src/components/model/companySystem/CompanySystemRanking/index.stories.tsx
+++ src/components/model/companySystem/CompanySystemRanking/index.stories.tsx
@@ -3,13 +3,14 @@ import { ComponentStory, ComponentMeta } from "@storybook/react"
 import Component from "./index"
 
 const companySystemList = new Array(5).fill(null).map((_, index) => ({
-  id: index,
+  id: String(index),
   name: "おひるねし放題制度",
   description:
     "業務時間内中に一度だけ昼寝する事を可能にする制度。二度寝は禁止で、昼寝開始から終了までの睡眠時間を業務時間として申請することができる。",
   thumbnailType: "2",
   author: "manaki",
   number: index,
+  createdAt: new Date("2022/01/12").toISOString(),
 }))
 
 export default {
