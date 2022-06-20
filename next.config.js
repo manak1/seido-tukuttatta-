d4d000b27917002fd06fabd293b4105f44d8727a
manak1 <mana23happy@hotmail.co.jp>
2022-06-21 00:08:51 +0900

    🐧 chore: mdファイルをimportできるように設定

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
   babel: {
     plugins: [["@babel/plugin-proposal-private-methods", { loose: true }]],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
