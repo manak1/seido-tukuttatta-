ff272865c1c62c1b3e91da75305a8fe3f2e815c6
manak1 <mana23happy@hotmail.co.jp>
2022-06-20 22:10:56 +0900

    🌟 feat: シェアのタグに制度名追加

--- src/utils/twitter.ts
+++ src/utils/twitter.ts
@@ -2,7 +2,7 @@ const BASE_URL = "https://twitter.com/intent/tweet?related=@mikeanakida&text="
 
 export const createdCompanySystemUrl = (name: string, url: string) => {
   return `${BASE_URL}${encodeURIComponent(
-    `株式会社ねむぺんに新しい制度「${name}」が誕生したよ！!\r\n #制度作ったったー ${url}`
+    `株式会社ねむぺんに新しい制度「${name}」が誕生したよ！!\r\n #制度作ったったー #${name} ${url}`
   )}`
 }
 
@@ -15,12 +15,12 @@ export const shareCompanySystemUrl = (
     `制度No${String(number).padStart(
       3,
       "0"
-    )}「${name}」\r\n #制度作ったったー ${url}`
+    )}「${name}」\r\n #制度作ったったー #${name} ${url}`
   )}`
 }
 
 export const createTwitterShareUrl = (name: string, url: string) => {
   return `${BASE_URL}${encodeURIComponent(
-    `株式会社ねむぺんに新しい制度「${name}」が誕生したよ！!\r\n #制度作ったったー ${url}`
+    `株式会社ねむぺんに新しい制度「${name}」が爆誕したよ！!\r\n #制度作ったったー #${name} ${url}`
   )}`
 }
