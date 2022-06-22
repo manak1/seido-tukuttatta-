ee3c7bb81e719b187cf195eb504105cd2d40f496
manak1 <mana23happy@hotmail.co.jp>
2022-06-23 01:40:35 +0900

    🔧 fix: Twitter共有のURL修正

--- src/utils/twitter.ts
+++ src/utils/twitter.ts
@@ -2,12 +2,13 @@ const BASE_URL = "https://twitter.com/intent/tweet?related=@mikeanakida&text="
 
 export const createdCompanySystemUrl = (name: string, url: string) => {
   return `${BASE_URL}${encodeURIComponent(
-    `株式会社ねむぺんに新しい制度「${name}」が誕生したよ！!\r\n\r\n#制度作ったったー #${name}\r\n`
+    `株式会社ねむぺんに新しい制度「${name}」が爆誕したよ！!\r\n\r\n#制度作ったったー #${name}\r\n`
   )}&url=${url}`
 }
 
 export const shareCompanySystemUrl = (name: string, url: string) => {
+  console.log(url)
   return `${BASE_URL}${encodeURIComponent(
-    `\r\n\r\n#制度作ったったー #${name}\r\n`
-  )}& url=${url} `
+    `\r\n#制度作ったったー #${name}\r\n`
+  )}&url=${url}`
 }
